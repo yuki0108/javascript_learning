6f46a73eca899ef7cec06527a12cd10d73cb6cb1
先規　勇樹 <yukisaki80@gmail.com>
2022-01-19 21:46:36 +0900

    initialcommit

---
+++ todo.js
@@ -0,0 +1,32 @@
+// ul要素の取得
+const todoUl = document.getElementById("todo-ul");
+
+// button要素の取得
+const enterButton = document.getElementById("enter-button");
+
+// button要素へのイベント割り付け
+enterButton.addEventListener("click", clickEnter);
+
+// クリック時に実行される関数
+function clickEnter() {
+  // li要素をJSで生成する
+  const todoLi = document.createElement("li");
+
+  // li要素に入れるテキストをinputに入力された値にする
+  todoLi.innerText = document.getElementById("todo-input").value;
+
+  // ul要素の中にliを入れる
+  todoUl.appendChild(todoLi);
+}
+
+// クリック時に実行される関数
+function clickEnter() {
+  // li要素をJSで生成する
+  const todoLi = document.createElement("li");
+  // li要素に入れるテキストをinputに入力された値にする
+  todoLi.innerText = document.getElementById("todo-input").value;
+  // ul要素の中にliを入れる
+  todoUl.appendChild(todoLi);
+  // inputの中身を空にする
+  document.getElementById("todo-input").value = "";
+}
